e42755471d453895c6585351c58918cc9100cf9b
Guilherme Hofstetter <91709904+AZALONhofs@users.noreply.github.com>
2024-02-28 12:17:24 -0300

    Add files via upload

---
+++ 28-02/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>variavel</title>   
+</head>
+<body>
+    <div class="texto">
+    <?php
+     $nome = "guilherme";
+     const sobrenome = "hofstetter";
+     $peso = "62";
+     $altura = "1,75";
+     $idade = "17";
+     echo  "meu nome é $nome " . sobrenome . " eu peso $peso kg, tenho $idade anos e meço 1,75 \u{1f60e} "
+    ?>
+    </div>
+</body>
+</html>
